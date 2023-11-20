09dc035be3dd5da7998678d63a5f65a23edb7665
Genic <sgtpain625@aol.com>
2023-11-19 20:58:36 -0500

    refinements

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
 
+//LOADING SCREEL//
 gsap.to("#loadingScreen", {
-    duration: 2, // Set the duration of the animation
+    duration: 3, // Set the duration of the animation
     y: "-100%", // Move the loading screen up to hide it
     ease: "power2.inOut", // Use ease for smooth animation
     onComplete: () => {
@@ -13,51 +14,48 @@ gsap.to("#loadingScreen", {
     }
   });
 
+//PARALAX//
+gsap.utils.toArray("section").forEach((section, i) => {
+section.bg = section.querySelector(".bg"); 
 
-  gsap.utils.toArray("section").forEach((section, i) => {
-    section.bg = section.querySelector(".bg"); 
-  
-    // Give the backgrounds some random images
-    // section.bg.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight}?random=${i})`;
-  
-    if (i) {
-      section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
-  
-      gsap.to(section.bg, {
-        backgroundPosition: `50% ${-innerHeight / 2}px`,
-        ease: "none",
-        scrollTrigger: {
-          trigger: section,
-          scrub: true
-        }
-      });
-    } 
-    
-    else {
-      section.bg.style.backgroundPosition = "50% 0px"; 
-  
-      gsap.to(section.bg, {
-        backgroundPosition: `50% ${-innerHeight / 2}px`,
-        ease: "none",
-        scrollTrigger: {
-          trigger: section,
-          start: "top top", 
-          end: "bottom top",
-          scrub: true
-        }
-      });
-    }
-  });
+if (i) {
+    section.bg.style.backgroundPosition = `50% ${innerHeight / 2}px`;
 
-
-  gsap.to("p", {
-    backgroundPositionX: "0%",
-    stagger: 1,
+    gsap.to(section.bg, {
+    backgroundPosition: `50% ${-innerHeight / 2}px`,
+    ease: "none",
     scrollTrigger: {
-      trigger: "#sec_1",
-      markers: true,
-      scrub: 1,
-      start: "top center",
-      end: "bottom center"
+        trigger: section,
+        scrub: true
     }
-  });
+    });
+} 
+
+else {
+    section.bg.style.backgroundPosition = "50% 0px"; 
+
+    gsap.to(section.bg, {
+    backgroundPosition: `50% ${-innerHeight / 2}px`,
+    ease: "none",
+    scrollTrigger: {
+        trigger: section,
+        start: "top top", 
+        end: "bottom top",
+        scrub: true
+    }
+    });
+}
+});
+
+//TEXT FILL//
+gsap.to("p", {
+backgroundPositionX: "0%",
+stagger: 1,
+scrollTrigger: {
+    trigger: "#sec_1",
+    markers: false,
+    scrub: 1,
+    start: "top center",
+    end: "bottom center"
+}
+});
